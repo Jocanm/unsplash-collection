ac72d27a69a8a4e51e657fd808f477811762d5c5
hytsigo <hytsigo1@gmail.com>
2025-07-10 16:22:57 -0500

    upgrading button

--- src/components/ui/CustomButton.tsx
+++ src/components/ui/CustomButton.tsx
@@ -1,9 +1,20 @@
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
+    variant?: "primary" | "secondary";
 }
-const CustomButton = ({ children }: ButtonProps) => {
+const CustomButton = ({
+    children,
+    variant = "primary",
+    ...props
+}: ButtonProps) => {
+    const basesStyle = "rounded-sm py-2 px-5 font-bevietnam";
+    const variantStyles =
+        variant === "primary"
+            ? "bg-[#E5E7EB] text-black hover:bg-gray-300"
+            : "bg-white text-[#6C727F] hover:bg-gray-200";
+
     return (
-        <button className="bg-[#E5E7EB] rounded-sm py-2 px-5 ">
+        <button className={`${basesStyle} ${variantStyles}`} {...props}>
             {children}
         </button>
     );
