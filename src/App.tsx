c7d56237cc455b44dc47a3e8e77de0cd2c917a3c
Juan Medina <juanmedi730@gmail.com>
2025-07-10 16:09:50 -0500

    feat: design input component

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,47 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import CustomButton from "./components/ui/CustomButton";
+import CustomInput from "./components/ui/CustomInput";
+import { Search } from "lucide-react";
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p className="font-outfit">
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-                <CustomButton>
-                    <p>Home</p>
-                </CustomButton>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+
+        <div>
+          <CustomInput
+            icon={<Search />}
+            type="text"
+            placeholder="Enter your keywords..."
+          />
+        </div>
+        <p className="font-outfit">
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <CustomButton>
+          <p>Home</p>
+        </CustomButton>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
 }
 
 export default App;
