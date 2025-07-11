4cbec3bd4ce80613fa411e173117baea01c6b755
Jose Angarita <amjose09@gmail.com>
2025-07-11 13:27:46 -0500

    search screen changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import "./App.css";
 import CustomInput from "./components/ui/CustomInput";
 import { Search } from "lucide-react";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center px-4 scroll-auto">
-      <h1 className="text-4xl text-[#121826] font-bold mb-2">Search</h1>
-      <p className="text-[#121826] mb-6">
-        Search high-resolution images from Unsplash
-      </p>
+    <div className="h-screen">
+      <div className="flex flex-col items-center justify-center h-full px-4">
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl text-[#121826] font-bold mb-2">Search</h1>
+          <p className="text-[#121826] mb-6 text-center">
+            Search high-resolution images from Unsplash
+          </p>
 
-      <div className="w-full max-w-md">
-        <CustomInput
-          icon={<Search />}
-          type="text"
-          placeholder="Enter your keywords..."
-        />
+          <div className="w-full sm:w-[528px]">
+            <CustomInput
+              icon={<Search />}
+              type="text"
+              placeholder="Enter your keywords..."
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
