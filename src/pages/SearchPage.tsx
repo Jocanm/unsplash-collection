e4988bff19e0e39aeed98fd2d7758a161a67d254
Jose Angarita <amjose09@gmail.com>
2025-07-15 11:13:13 -0500

    fixing layout

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -8,7 +8,7 @@ export const SearchPage = () => {
     useSearchImages();
 
   return (
-    <div className="flex flex-col items-center border h-full justify-center px-4">
+    <div className="flex flex-col items-center justify-center px-4 overflow-y-visible flex-auto">
       <form className="w-full sm:w-[528px]" onSubmit={handleSubmit}>
         <h1 className="text-4xl text-[#121826] font-bold mb-2 text-center">
           Search
