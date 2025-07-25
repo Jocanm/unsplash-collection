652f28ed30bf53da55dc8f579b3b8ef894481ae3
Jose Angarita <amjose09@gmail.com>
2025-07-25 14:56:31 -0500

    fixing things

--- src/components/ui/GridSkeleton.tsx
+++ src/components/ui/GridSkeleton.tsx
@@ -1,7 +1,9 @@
+const items = Array.from({ length: 20 });
+
 export const GridSkeleton = () => {
   return (
     <>
-      {Array.from({ length: 20 }).map((_, i) => (
+      {items.map((_, i) => (
         <div
           key={`skeleton-${i}`}
           className="mb-4 w-full h-[250px] bg-gray-200 rounded-lg animate-pulse"
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,21 +1,16 @@
-import { useSearchImages } from "../hooks/useSearchImages";
 import { Search } from "lucide-react";
 import CustomInput from "../components/ui/CustomInput";
-import { useState } from "react";
 import { GridSkeleton } from "../components/ui/GridSkeleton";
+import { useSearchImages } from "../hooks/useSearchImages";
 
 export const SearchPage = () => {
-  const {
-    textValue,
-    setTextValue,
-    handleSubmit,
-    searchResult,
-  } = useSearchImages();
-  const [isLoading] = useState(false);
-  const isResultsReady = searchResult?.data && !isLoading;
+  const { textValue, setTextValue, handleSubmit, searchResult } =
+    useSearchImages();
+  const isResultsReady = searchResult?.data && !searchResult.isLoading;
 
   return (
     <>
+      {/* TODO: componente aparte */}
       {isResultsReady && (
         <div className="absolute flex top-[65px] z-0 h-20 w-full bg-gradient-to-r from-orange-300 to-purple-600 opacity-100 translate-y-4 animate-fade-up" />
       )}
@@ -23,12 +18,18 @@ export const SearchPage = () => {
       <div
         className={`flex flex-col pt-[90px] items-center justify-center px-6 overflow-y-visible flex-auto z-1 
         transition-all duration-500 ease-in-out
-        ${isResultsReady ? "translate-y-[-40px] opacity-100" : "translate-y-0 opacity-100"}`}
+        ${
+          isResultsReady
+            ? "translate-y-[-40px] opacity-100"
+            : "translate-y-0 opacity-100"
+        }`}
       >
+        {/* TODO: componente aparte */}
         <form
           className="w-full sm:w-[528px] bg-white rounded-[8px]"
           onSubmit={handleSubmit}
         >
+          {/* TODO: componente aparte */}
           {!isResultsReady && (
             <>
               <h1 className="text-4xl text-[#121826] font-bold mb-2 text-center">
@@ -49,14 +50,15 @@ export const SearchPage = () => {
           />
         </form>
 
+        {/* TODO: componente aparte */}
         <div className="columns-1 sm:columns-2 lg:columns-4 gap-7 px-[42px] py-12 w-full">
-          {isLoading || !searchResult?.data ? (
+          {searchResult.isLoading ? (
             <GridSkeleton />
           ) : (
-            searchResult.data.map((image) => (
+            searchResult.data?.map((image) => (
               <div
                 key={image.id}
-                className="mb-4 transition transform duration-300 ease-in hover:-translate-y-1 hover:scale-110"
+                className="mb-4 transition transform duration-300 ease-in cursor-pointer"
               >
                 <img
                   src={image.regularUrl}
